6b01b9bf176206729657da1ed4cba3950d21d2f5
Lukas Werner <kontakt@lwerner.de>
2020-10-12 19:54:02 +0200

    Finalize requests/responses

--- flask_application/views.py
+++ flask_application/views.py
@@ -1,15 +1,22 @@
-from flask import render_template
+from flask import redirect, render_template, request
 
 from . import app
+
+search_engines = {
+    'Google': 'https://google.com',
+    'Yahoo': 'https://yahoo.com',
+    'DuckDuckGo': 'https://duckduckgo.com',
+    'Yandex': 'https://yandex.com',
+}
 
 
 @app.route('/')
 def index():
-    search_engines = {
-        'Google': 'https://google.com',
-        'Yahoo': 'https://yahoo.com',
-        'DuckDuckGo': 'https://duckduckgo.com',
-        'Yandex': 'https://yandex.com',
-    }
-
     return render_template('index.html', name='World', search_engines=search_engines)
+
+
+@app.route('/add', methods=['POST'])
+def add_search_engine():
+    search_engines[request.form.get('search-engine-name')] = request.form.get('search-engine-url')
+
+    return redirect('/')
